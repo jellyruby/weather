6aea003025455ca43e21635874de72dc95dcd230
jellyruby <50604410+jellyruby@users.noreply.github.com>
2022-11-25 01:32:54 +0900

    Update utils.js

--- myapp/src/assets/js/utils.js
+++ myapp/src/assets/js/utils.js
@@ -18,7 +18,7 @@ const preloadImages = (selector = 'img') => {
     return new Promise((resolve) => {
         WebFont.load({
             google: {
-                families: ['Poor Story']
+                families: ['Dongle']
               }
             /*typekit: {
                 id: id
